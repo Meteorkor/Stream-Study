acd9a178bd19713cc00e3771d01b44b1ddb71192
unseok.kim <unseok.kim@linecorp.com>
2022-07-13 22:35:24 +0900

    add parallelKeepOrderTest

--- src/test/java/com/meteor/stream/create/StreamParallelCreateTest.java
+++ src/test/java/com/meteor/stream/create/StreamParallelCreateTest.java
@@ -1,15 +1,37 @@
 package com.meteor.stream.create;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 import java.io.BufferedReader;
 import java.io.CharArrayReader;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class StreamParallelCreateTest {
+    @Test
+    void parallelKeepOrderTest() {
+        final List<String> collect = IntStream.range(0, 10000).mapToObj(String::valueOf)
+                                              .collect(Collectors.toList());
+        Collections.shuffle(collect);
+
+        final List<String> newList = collect.parallelStream()
+                                            .map(item -> {
+                                                //work something
+                                                return item + "";
+                                            })
+                                            .map(Function.identity())
+                                            .collect(Collectors.toList());
+
+        Assertions.assertNotEquals(System.identityHashCode(collect), System.identityHashCode(newList));
+        Assertions.assertEquals(collect, newList);
+    }
+
     @Test
     void stringStreamParallelTest() {
         StringBuilder stb = new StringBuilder("line0");
@@ -23,9 +45,9 @@ public class StreamParallelCreateTest {
             try (BufferedReader bufferedReader = new BufferedReader(charArrayReader)) {
 
                 long cnt = bufferedReader.lines()
-                        .parallel()
+                                         .parallel()
 //                        .peek(s -> System.out.println("line : " + s))
-                        .count();
+                                         .count();
                 System.out.println("cnt : " + cnt);
                 Assertions.assertEquals(100000, cnt);
                 //100000
@@ -40,28 +62,27 @@ public class StreamParallelCreateTest {
         //int라 합계가 초과하지만, int 갯수를 늘리기 위해 어쩔수 없음
         int endInt = 100000;
         int asInt = IntStream.rangeClosed(1, endInt)
-                .parallel()
-                .reduce((num1, num2) -> {
+                             .parallel()
+                             .reduce((num1, num2) -> {
 //                    System.out.println(String.format(Thread.currentThread() + "] num1 : %d, num2 : %d", num1, num2));
-                    return num1 + num2;
-                })
-                .orElseThrow(()->new NoSuchElementException(""));
+                                 return num1 + num2;
+                             })
+                             .orElseThrow(() -> new NoSuchElementException(""));
 
         Assertions.assertEquals((endInt) * (endInt + 1) / 2, asInt);
     }
-
 
     @Test
     void intBoxStreamParallelTest() {
         //int라 합계가 초과하지만, int 갯수를 늘리기 위해 어쩔수 없음
         int endInt = 100000;
         int asInt = IntStream.rangeClosed(1, endInt)
-                .boxed().parallel()
-                .reduce((num1, num2) -> {
+                             .boxed().parallel()
+                             .reduce((num1, num2) -> {
 //                    System.out.println(String.format(Thread.currentThread() + "] num1 : %d, num2 : %d", num1, num2));
-                    return num1 + num2;
-                })
-                .orElseThrow(()->new NoSuchElementException(""));
+                                 return num1 + num2;
+                             })
+                             .orElseThrow(() -> new NoSuchElementException(""));
 
         Assertions.assertEquals((endInt) * (endInt + 1) / 2, asInt);
     }
