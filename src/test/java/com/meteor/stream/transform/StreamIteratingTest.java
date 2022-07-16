2663c6a439e4642e1070c69ada7cef4354fe8bf3
unseok.kim <unseok.kim@linecorp.com>
2022-07-16 20:39:13 +0900

    add peekNotWorkingTest

--- src/test/java/com/meteor/stream/transform/StreamIteratingTest.java
+++ src/test/java/com/meteor/stream/transform/StreamIteratingTest.java
@@ -1,12 +1,14 @@
 package com.meteor.stream.transform;
 
-import com.meteor.stream.dto.EmpDto;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
+import com.meteor.stream.dto.EmpDto;
 
 public class StreamIteratingTest {
 
@@ -19,16 +21,16 @@ public class StreamIteratingTest {
                         , EmpDto.builder().empno(2).ename("lee").salary(100).build()
                         , EmpDto.builder().empno(3).ename("park").salary(500).build());
                 long count = empList.stream()
-                        .map(s -> {
-                            System.out.println("map.s : " + s);
-                            s.setSalary(s.getSalary() * 2);
-                            return s;
-                        })
-                        .peek(s -> {
-                            if ("kim".equals(s.getEname())) throw new RuntimeException();
-                            System.out.println("s : " + s);
-                        })
-                        .mapToLong(s -> s.getSalary()).sum();
+                                    .map(s -> {
+                                        System.out.println("map.s : " + s);
+                                        s.setSalary(s.getSalary() * 2);
+                                        return s;
+                                    })
+                                    .peek(s -> {
+                                        if ("kim".equals(s.getEname())) {throw new RuntimeException();}
+                                        System.out.println("s : " + s);
+                                    })
+                                    .mapToLong(s -> s.getSalary()).sum();
                 Assertions.fail("sum은 map과 peek를 호출한다.");
             } catch (RuntimeException t) {
                 //is ok
@@ -46,22 +48,37 @@ public class StreamIteratingTest {
                     , EmpDto.builder().empno(2).ename("lee").salary(100).build()
                     , EmpDto.builder().empno(3).ename("park").salary(500).build());
             long count = empList.stream()
-                    .map(s -> {
-                        System.out.println("map.s : " + s);
-                        s.setSalary(s.getSalary() * 2);
-                        return s;
-                    })
-                    .peek(s -> {
-                        //local 테스트에서는 발생안하고
-                        //travis에서는 exception이 발생하네..?;;;
-                        //하긴 위에 terminal command에 따라 가운데 로직을 돌리고 안돌리고를 미리 예측하는건..
+                                .map(s -> {
+                                    System.out.println("map.s : " + s);
+                                    s.setSalary(s.getSalary() * 2);
+                                    return s;
+                                })
+                                .peek(s -> {
+                                    //local 테스트에서는 발생안하고
+                                    //travis에서는 exception이 발생하네..?;;;
+                                    //하긴 위에 terminal command에 따라 가운데 로직을 돌리고 안돌리고를 미리 예측하는건..
 //                        if ("kim".equals(s.getEname())) throw new RuntimeException();
 //                        System.out.println("s : " + s);
-                    }).count();
+                                }).count();
 
 //            long count = empList.stream().peek(System.out::println).count();
             System.out.println("count : " + count);
             Assertions.assertEquals(3, count);
         }
     }
+
+    @Test
+    @DisplayName("peek는 terminal 종료으로 마무리 되지 않으면 peek는 수행되지 않는다.")
+    void peekNotWorkingTest() {
+        AtomicBoolean isWorking = new AtomicBoolean(false);
+        List<EmpDto> empList = Arrays.asList(EmpDto.builder().empno(1).ename("kim").salary(100).build()
+                , EmpDto.builder().empno(2).ename("lee").salary(100).build()
+                , EmpDto.builder().empno(3).ename("park").salary(500).build());
+        empList.stream()
+               .peek(dto -> {
+                   isWorking.set(true);
+               });
+        Assertions.assertFalse(isWorking.get());
+    }
+
 }
